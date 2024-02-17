4b2161629851b86b032a63f34acdf75f0d68c268
Martin Geisse <martingeisse@googlemail.com>
2024-02-17 17:25:46 +0100

    .....

--- backend/src/gitCommit.ts
+++ backend/src/gitCommit.ts
@@ -1,1 +1,1 @@
-export const gitCommit = "dd52a2a444f30027f14082164caeed25c9428e94";
+export const gitCommit = "68f96c2757f9318225d5f3b1a904e72daf6e5ee9";
--- content/src/index.ts
+++ content/src/index.ts
@@ -1,22 +1,44 @@
 import "source-map-support/register";
-import { createCanvas } from "canvas";
-import { writeFileSync } from "fs";
+import {createCanvas} from "canvas";
+import {base64Encode} from "./util/base64";
+import axios, {AxiosRequestConfig} from "axios";
 
-const canvas = createCanvas(200, 200, "svg");
-const context = canvas.getContext("2d");
+async function updateImage(imageId: number, width: number, height: number, renderer: (context: CanvasRenderingContext2D) => void) {
+    const canvas = createCanvas(width, height);
+    const context = canvas.getContext("2d");
+    renderer(context as any);
+    const request = {
+        contentType: "image/png",
+        dataBase64: base64Encode(canvas.toBuffer("image/png")),
+    };
+    const config: AxiosRequestConfig = {
+        headers: {
+            Authorization: "Bearer 1",
+        },
+    };
+    const response = await axios.put(`http://localhost:8080/author/updateImage/${imageId}`, request, config);
+    if (response.status !== 200) {
+        throw new Error(`got unexpected status code ${response.status}`);
+    }
+}
 
-// Write "Awesome!"
-context.font = "30px Impact";
-context.rotate(0.1);
-context.fillText('Awesome!', 50, 100);
+async function main() {
+    await updateImage(1, 200, 200, (context) => {
 
-// Draw line under text
-const textMetrics = context.measureText("Awesome!");
-context.strokeStyle = "rgba(0,0,0,0.5)";
-context.beginPath();
-context.lineTo(50, 102);
-context.lineTo(50 + textMetrics.width, 102);
-context.stroke();
+        // Write "Awesome!"
+        context.font = "30px Impact";
+        context.rotate(0.1);
+        context.fillText('Awesome!', 50, 100);
 
-const buffer = canvas.toBuffer("image/png");
-writeFileSync("test.png", buffer);
+        // Draw line under text
+        const textMetrics = context.measureText("Awesome!");
+        context.strokeStyle = "rgba(0,0,0,0.5)";
+        context.beginPath();
+        context.lineTo(50, 102);
+        context.lineTo(50 + textMetrics.width, 102);
+        context.stroke();
+
+    });
+}
+
+main().then(() => {});
