4ebc2be068339cdf37b5ae33da48158a2ceea124
Martin Geisse <martingeisse@googlemail.com>
2024-03-17 14:43:02 +0100

    .....

--- backend/src/self/util/postgres/postgresPool.ts
+++ backend/src/self/util/postgres/postgresPool.ts
@@ -1,6 +1,11 @@
-import {Pool, types} from "pg";
+import {Pool, PoolClient, QueryResult, QueryResultRow, types} from "pg";
 import {backendSystemConfiguration} from "../../backendSystemConfiguration";
 
+// --------------------------------------------------------------------------------------------------------------------
+// type mapping
+// --------------------------------------------------------------------------------------------------------------------
+
+// map type 20 (bigint)
 types.setTypeParser(20, (val) => {
   const result = parseInt(val, 10);
   if (result >= Number.MAX_SAFE_INTEGER) {
@@ -8,6 +13,10 @@ types.setTypeParser(20, (val) => {
   }
   return result;
 });
+
+// --------------------------------------------------------------------------------------------------------------------
+// connection pool
+// --------------------------------------------------------------------------------------------------------------------
 
 let poolPromise: Promise<Pool> | null = null;
 
@@ -26,4 +35,53 @@ export function getPostgresPool(): Promise<Pool> {
     })();
   }
   return poolPromise;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+// connection wrapper class
+// --------------------------------------------------------------------------------------------------------------------
+
+export interface PostgresConnection {
+  needsInternalClient(): PoolClient;
+  query<R extends QueryResultRow>(queryText: string, values?: unknown[]): Promise<QueryResult<R>>;
+  release(): void;
+}
+
+class PostgresConnectionImpl implements PostgresConnection {
+  constructor(private internalClient: PoolClient | null) {}
+
+  needsInternalClient(): PoolClient {
+    if (this.internalClient === null) {
+      throw new Error("Postgres client has already been released");
+    }
+    return this.internalClient;
+  }
+
+  async query<R extends QueryResultRow>(queryText: string, values?: unknown[]): Promise<QueryResult<R>> {
+    return this.needsInternalClient().query(queryText, values);
+  }
+
+  release(): void {
+    this.needsInternalClient().release();
+    this.internalClient = null;
+  }
+
+  checkReleased(): void {
+    if (this.internalClient !== null) {
+      this.internalClient.release();
+      this.internalClient = null;
+      console.error("Postgres client has not been released after TTL");
+    }
+  }
+}
+
+export async function getNewGlobalPostgresConnection(
+    autoCloseTtlSeconds: number | null = 600,
+): Promise<PostgresConnection> {
+  const internalClient: PoolClient = await (await getPostgresPool()).connect();
+  const connection = new PostgresConnectionImpl(internalClient);
+  if (autoCloseTtlSeconds !== null) {
+    setTimeout(() => connection.checkReleased(), autoCloseTtlSeconds * 1000);
+  }
+  return connection;
 }
