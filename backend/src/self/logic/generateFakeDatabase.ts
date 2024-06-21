6e235f0e4749fee4b4faa2b54659ef7197a551f1
Martin Geisse <martingeisse@googlemail.com>
2024-06-21 19:52:31 +0200

    .....

--- backend/src/self/logic/generateFakeDatabase.ts
+++ backend/src/self/logic/generateFakeDatabase.ts
@@ -5,14 +5,18 @@ import {Pool} from "pg";
 async function tableToString(pool: Pool, name: string, fields: string[]): Promise<string> {
     const sql = `SELECT ${fields.map(x => `"${x}"`).join(", ")} FROM "edu2"."${name}" ORDER BY "id"`;
     const rows = (await pool.query(sql, [] as unknown[])).rows;
-    return `${name}: ${JSON.stringify(rows, null, 2)},\n\n`;
+    return `export const fakeDatabaseTable_${name}: FakeDatabase${name}Record[] = ${JSON.stringify(rows, null, 2)};\n\n`;
 }
 
 export async function generateFakeDatabase(): Promise<void> {
     const postgresPool = await getPostgresPool();
-    let fileContents = "export const databaseContents = {\n\n";
+    let fileContents = `import {
+    FakeDatabaseAuthorRecord,
+    FakeDatabaseCourseRecord,
+    FakeDatabaseUnitRecord,
+    FakeDatabaseImageRecord,    
+} from \"./fake-database-types\";\n\n`;
     fileContents += await tableToString(postgresPool, "Author", ["id", "name"]);
     fileContents += await tableToString(postgresPool, "Course", ["id", "authorId", "title", "description", "scriptLibrary"]);
-    fileContents += "}\n";
     await writeFile("../common/src/self/fake-database/fake-database-contents.ts", fileContents);
 }
