c6826349c98cc7c9bee48a7e5b626b40f14a5b15
Martin Geisse <martingeisse@googlemail.com>
2024-06-21 16:34:17 +0200

    .....

--- backend/src/self/logic/writeLiteralDatabaseContentsToFile.ts
+++ backend/src/self/logic/writeLiteralDatabaseContentsToFile.ts
@@ -3,7 +3,7 @@ import {writeFile} from "fs/promises";
 import {Pool} from "pg";
 
 async function tableToString(pool: Pool, name: string, fields: string[]): Promise<string> {
-    const sql = `SELECT ${fields.map(f => `, "${f}"`).join("")} FROM "${name}" ORDER BY "id"`;
+    const sql = `SELECT ${fields.map(x => `"${x}"`).join(", ")} FROM "edu2"."${name}" ORDER BY "id"`;
     const rows = (await pool.query(sql, [] as unknown[])).rows;
     return `${name}: ${JSON.stringify(rows, null, 2)},\n\n`;
 }
@@ -11,8 +11,8 @@ async function tableToString(pool: Pool, name: string, fields: string[]): Promis
 export async function writeLiteralDatabaseContentsToFile(): Promise<void> {
     const postgresPool = await getPostgresPool();
     let fileContents = "export const databaseContents = {\n\n";
-    fileContents += await tableToString(postgresPool, "edu2.Author", ["id", "name"]);
-    fileContents += await tableToString(postgresPool, "edu2.Course", ["id", "authorId", "title", "description", "scriptLibrary"]);
+    fileContents += await tableToString(postgresPool, "Author", ["id", "name"]);
+    fileContents += await tableToString(postgresPool, "Course", ["id", "authorId", "title", "description", "scriptLibrary"]);
     fileContents += "}\n";
     await writeFile("src/databaseContents.ts", fileContents);
 }
