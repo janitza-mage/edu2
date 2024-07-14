d71457b9b8689ef511b58a2e5bd974248231116c
Martin Geisse <martingeisse@googlemail.com>
2024-07-14 11:21:44 +0200

    .....

--- frontend-content/src/self/App/AppRoutes.tsx
+++ frontend-content/src/self/App/AppRoutes.tsx
@@ -1,13 +1,13 @@
 import {Route, Routes, useParams} from "react-router-dom";
-import {ExerciseSheet} from "../ExerciseSheet/ExerciseSheet";
+import {UnitPageContent} from "../ExerciseSheet/UnitPageContent";
 
-function ExerciseSheetWrapper() {
+function UnitPageContentWrapper() {
     let {courseId, unitIndex} = useParams();
-    return <ExerciseSheet key={courseId} courseId={parseInt(courseId!, 10)} unitIndex={parseInt(unitIndex!, 10)} />;
+    return <UnitPageContent key={courseId} courseId={parseInt(courseId!, 10)} unitIndex={parseInt(unitIndex!, 10)} />;
 }
 
 export function AppRoutes() {
     return <Routes>
-        <Route path={"/:courseId/:unitIndex"} element={<ExerciseSheetWrapper />} />;
+        <Route path={"/:courseId/:unitIndex"} element={<UnitPageContentWrapper />} />;
     </Routes>;
 }
--- frontend-content/src/self/ExerciseSheet/UnitPageContent.tsx
+++ frontend-content/src/self/ExerciseSheet/UnitPageContent.tsx
@@ -1,6 +1,6 @@
 import {backendGet} from "../util/backendGet";
 import {ExerciseDefinitionSwitch} from "./ExerciseDefinitionSwitch";
-import {GetExerciseResponse} from "../../common/frontend-api/GetExerciseResponse";
+import {GetUnitPageContentResponse} from "../../common/frontend-api/GetUnitPageContentResponse";
 import {exerciseDefinitionSchema} from "../../common/types/ExerciseDefinition";
 import {getErrorMessage} from "../../common/util/getErrorMessage";
 import {useLoader} from "../../uilib/util/useLoader";
@@ -12,8 +12,8 @@ export interface ExerciseSheetProps {
     unitIndex: number;
 }
 
-async function loadExercise(props: ExerciseSheetProps): Promise<GetExerciseResponse> {
-    const response = (await backendGet(`getExercise/${props.courseId}/${props.unitIndex}`)) as GetExerciseResponse;
+async function loadExercise(props: ExerciseSheetProps): Promise<GetUnitPageContentResponse> {
+    const response = (await backendGet(`getExercise/${props.courseId}/${props.unitIndex}`)) as GetUnitPageContentResponse;
     try {
         exerciseDefinitionSchema.parse(response.exerciseDefinition);
     } catch (error) {
@@ -23,7 +23,7 @@ async function loadExercise(props: ExerciseSheetProps): Promise<GetExerciseRespo
     return response;
 }
 
-export function ExerciseSheet(props: ExerciseSheetProps) {
+export function UnitPageContent(props: ExerciseSheetProps) {
     const loader = useLoader(() => loadExercise(props));
     return <FullWidthLoadingIndicator loader={loader}>
         {(result) => <>
