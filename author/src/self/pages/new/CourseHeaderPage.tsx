cc37b64d947cc48225189fd9216c8f2c7581d5c6
Martin Geisse <martingeisse@googlemail.com>
2024-08-09 13:54:43 +0200

    .....

--- author/src/self/pages/new/CourseHeaderPage.tsx
+++ author/src/self/pages/new/CourseHeaderPage.tsx
@@ -2,6 +2,8 @@ import {getBackendCourseAndUnits} from "../../logic/backend/backend";
 import React from "react";
 import {useLoader} from "../../../uilib/util/useLoader";
 import {CourseSidebarHelper} from "./CourseSidebar";
+import {FullWidthLoadingIndicator} from "../../../uilib/LoadingIndicator/FullWidthLoadingIndicator";
+import {CourseHeaderDataPanel} from "../panels/CourseHeaderDataPanel";
 
 interface CourseHeaderPageProps {
     courseId: number;
@@ -10,6 +12,17 @@ interface CourseHeaderPageProps {
 export function CourseHeaderPage(props: CourseHeaderPageProps) {
     const loader = useLoader(() => getBackendCourseAndUnits(props.courseId));
     return <CourseSidebarHelper courseId={props.courseId} loader={loader} subpageSelector={null}>
-        header
+        <FullWidthLoadingIndicator loader={loader}>
+            {result => <>
+                <h1>Course</h1>
+                <CourseHeaderDataPanel
+                    courseId={props.courseId}
+                    title={result.title}
+                    description={result.description}
+                    scriptLibrary={result.scriptLibrary}
+                    onDataUpdatedInBackend={() => loader.reload(() => getBackendCourseAndUnits(props.courseId))}
+                />
+            </>}
+        </FullWidthLoadingIndicator>
     </CourseSidebarHelper>;
 }
