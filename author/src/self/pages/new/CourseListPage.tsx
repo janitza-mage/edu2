295ec701fb7e2efb90cf7611a4138618ee4b8bd9
Martin Geisse <martingeisse@googlemail.com>
2024-08-09 14:35:26 +0200

    .....

--- author/src/self/pages/new/CourseListPage.tsx
+++ author/src/self/pages/new/CourseListPage.tsx
@@ -1,29 +1,24 @@
 import {getBackendCourseList} from "../../logic/backend/backend";
-import {
-    GetBackendCourseListResponse,
-    GetBackendCourseListResponseElement
-} from "../../../common/author-api/GetBackendCourseListResponse";
+import {GetBackendCourseListResponse} from "../../../common/author-api/GetBackendCourseListResponse";
 import React from "react";
 import {useLoader} from "../../../uilib/util/useLoader";
-import {InlineMarkdownButtonList} from "../../components/InlineMarkdownButtonList";
-import {useNavigate} from "react-router-dom";
 import {FullWidthLoadingIndicator} from "../../../uilib/LoadingIndicator/FullWidthLoadingIndicator";
 import {PageFrame} from "./PageFrame";
+import {InlineMarkdownLinkButtonList} from "../../components/InlineMarkdownLinkButtonList";
 
 interface CourseListPanelProps {
     response: GetBackendCourseListResponse;
 }
 
 function CourseListPanel(props: CourseListPanelProps) {
-    const navigate = useNavigate();
     return <>
         <h1>Courses</h1>
-        <InlineMarkdownButtonList
+        <InlineMarkdownLinkButtonList
             elements={props.response.courses}
             courseIdForImages={null}
             keyMapper={course => course.courseId}
             labelMapper={course => course.title}
-            onClick={(course: GetBackendCourseListResponseElement) => navigate(`/courses/${course.courseId}`)}
+            buildToAttribute={course => `/courses/${course.courseId}`}
         />
     </>;
 }
