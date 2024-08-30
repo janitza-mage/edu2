2770ba5109928fc377398ce6392da8bd940f4da1
Martin Geisse <martingeisse@googlemail.com>
2024-08-30 06:47:15 +0200

    .....

--- content/src/self/index.ts
+++ content/src/self/index.ts
@@ -49,20 +49,31 @@ async function main() {
         point(context, 2, 3);
     });
     */
-    await updateImage(9, 200, 200, (context) => {
+    await updateImage(1, 200, 200, (context) => {
         setupCoordinateGridCanvas(context, -0.6, 6, -0.6, 6, { clipNegative: true });
     });
-    await updateImage(10, 200, 200, (context) => {
+    await updateImage(2, 200, 200, (context) => {
         setupCoordinateGridCanvas(context, -0.6, 6, -0.6, 6, { clipNegative: true });
         context.fillStyle = "black";
         point(context, 1, 3);
     });
-    await updateImage(11, 200, 200, (context) => {
+    await updateImage(3, 200, 200, (context) => {
         setupCoordinateGridCanvas(context, -0.6, 6, -0.6, 6, { clipNegative: true });
+        
+        context.strokeStyle = "#0f0";
+        context.lineWidth = 0.1;
+        context.beginPath();
+        context.moveTo(1, 0);
+        context.lineTo(1, 6.5);
+        context.moveTo(0, 3);
+        context.lineTo(6.5, 3);
+        context.stroke();
+
         context.fillStyle = "black";
         point(context, 1, 3, "(1|3)");
+        
     });
-    await updateImage(12, 200, 200, (context) => {
+    await updateImage(4, 200, 200, (context) => {
         setupCoordinateGridCanvas(context, -0.6, 6, -0.6, 6, { clipNegative: true });
         context.fillStyle = "black";
         const pointOptions = { font: "0.5px sans-serif", labelX: 0.2, labelY: 0.5 };
@@ -81,7 +92,7 @@ async function main() {
         showText(context, 2, 2.1, "Abstand: 4");
 
     });
-    await updateImage(13, 200, 200, (context) => {
+    await updateImage(5, 200, 200, (context) => {
         setupCoordinateGridCanvas(context, -0.6, 6, -0.6, 6, { clipNegative: true });
         context.fillStyle = "black";
         const pointOptions = { font: "0.5px sans-serif", labelX: 0.2, labelY: 0.5 };
@@ -98,6 +109,63 @@ async function main() {
 
         context.font = "0.4px sans-serif";
         showText(context, 1.1, 2.8, "Abstand: 2");
+
+    });
+    await updateImage(6, 200, 200, (context) => {
+        setupCoordinateGridCanvas(context, -0.6, 6, -0.6, 6, { clipNegative: true });
+
+        context.fillStyle = context.strokeStyle = "red";
+        point(context, 1, 1);
+        point(context, 2, 3);
+        context.setLineDash([]);
+        context.beginPath();
+        context.moveTo(1, 1);
+        context.lineTo(2, 3);
+        context.stroke();
+
+        context.fillStyle = context.strokeStyle = "blue";
+        point(context, 4, 2);
+        point(context, 6, 6);
+        context.setLineDash([0.1]);
+        context.beginPath();
+        context.moveTo(4, 2);
+        context.lineTo(6, 6);
+        context.stroke();
+
+    });
+    await updateImage(7, 200, 200, (context) => {
+        setupCoordinateGridCanvas(context, -0.6, 6, -0.6, 6, { clipNegative: true });
+
+        context.fillStyle = context.strokeStyle = "red";
+        context.setLineDash([]);
+        context.beginPath();
+        context.moveTo(1, 1);
+        context.lineTo(2, 3);
+        context.stroke();
+
+        context.fillStyle = context.strokeStyle = "blue";
+        context.setLineDash([0.1]);
+        context.beginPath();
+        context.moveTo(1, 1);
+        context.lineTo(3, 5);
+        context.stroke();
+
+        context.fillStyle = context.strokeStyle = "#cc0";
+        context.setLineDash([]);
+        context.beginPath();
+        context.moveTo(1, 1);
+        context.lineTo(3, 2);
+        context.stroke();
+        
+        // draw the points last so they aren't covered by the lines
+        context.fillStyle = context.strokeStyle = "green";
+        point(context, 1, 1);
+        context.fillStyle = context.strokeStyle = "red";
+        point(context, 2, 3);
+        context.fillStyle = context.strokeStyle = "blue";
+        point(context, 3, 5);
+        context.fillStyle = context.strokeStyle = "#cc0";
+        point(context, 3, 2);
 
     });
 }
