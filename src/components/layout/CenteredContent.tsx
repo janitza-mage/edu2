264c372dea899d88355e8218158ec16060258055
Martin Geisse <martingeisse@googlemail.com>
2025-01-03 18:36:33 +0100

    uni

--- src/components/layout/CenteredContent.tsx
+++ src/components/layout/CenteredContent.tsx
@@ -6,6 +6,7 @@ export interface CenteredContentProps {
 }
 
 export function CenteredContent(props: CenteredContentProps) {
+    // TODO handle content that is too large
     return <div style={{width: (props.widthPercent ?? 50) + "%", height: "100%", marginLeft: "auto", marginRight: "auto", position: "relative"}}>
         <div style={{width: "100%", position: "absolute", top: "50%", transform: "translateY(-50%)"}}>
             {props.children}
