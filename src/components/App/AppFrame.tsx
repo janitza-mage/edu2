79c7ddfaf6f75b22c6176a0598b69d75c440611a
Martin Geisse <martingeisse@googlemail.com>
2025-01-27 09:36:13 +0100

    uni

--- src/components/App/AppFrame.tsx
+++ src/components/App/AppFrame.tsx
@@ -6,6 +6,7 @@ export interface AppFrameProps {
 }
 
 export function AppFrame({children}: AppFrameProps): ReactElement {
+    console.log("rendering AppFrame");
     return <div className={styles.AppFrame}>
         {children}
     </div>;
