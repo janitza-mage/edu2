49ecfd78329b952b86436cdc51125adebdaf837f
Martin Geisse <martingeisse@googlemail.com>
2025-01-27 08:15:43 +0100

    uni

--- src/pages/content/ContentPathPage.tsx
+++ src/pages/content/ContentPathPage.tsx
@@ -8,7 +8,7 @@ export interface ContentPathPageProps {
 export function ContentPathPage(props: ContentPathPageProps) {
     let node = getContentNodeByPath(props.contentPath);
     if (!node) {
-        return <div>node not found</div>;
+        return <div>node not found: {props.contentPath.join("/")}</div>;
     }
     return <ContentNodePage node={node} path={props.contentPath} />;
 }
