ea1fcdb3072f916cbb80d243d848a90dae3aac4f
Martin Geisse <martingeisse@googlemail.com>
2025-02-10 09:53:07 +0100

    uni

--- src/content/analysis/induktion/induktionUnit6-summe-ungerade.tsx
+++ src/content/analysis/induktion/induktionUnit6-summe-ungerade.tsx
@@ -1,23 +1,31 @@
 import {createSteppedUnit} from "../../../unit/step/createSteppedUnit";
 import {createReadStep} from "../../../unit/read/createReadStep";
+import {MathTable} from "./MathTable";
+import {createNumberKeyboardExercise} from "../../../unit/numbers/createNumberKeyboardExercise";
+import {mathDiv, mathSpan} from "../../../components/Math/Math";
 
 export const induktionUnit6 = createSteppedUnit("summeUngerade", "Summe der ersten n ungeraden Zahlen", () => [
     createReadStep({
-        content: <pre>
-Es soll gezeigt werden:
-Die Summe der ersten n ungeraden Zahlen ist n^2
-    1             = 1
-    1 + 3         = 4
-    1 + 3 + 5     = 9
-    1 + 3 + 5 + 7 = 16
-    Summe(i=1..n)(2i-1) = n^2
-            </pre>,
+        content: <>
+            <p>Es soll gezeigt werden: Die Summe der ersten {mathSpan("n")} ungeraden Zahlen ist {mathSpan("n^2")}.</p>
+            <p>
+                <MathTable widthPercent={60} headers={["n", "1+3+...+(2n-1)", "n^2"]} cells={[
+                    ["1", "1", "1"],
+                    ["2", "1+3=4", "4"],
+                    ["3", "1+3+5=9", "9"],
+                    ["4", "1+3+5+7=16", "16"],
+                    ["5", "1+3+5+7+9=25", "25"],
+                ]} />
+            </p>
+        </>,
     }),
-    createReadStep({
-        content: <pre>
-Die Summe der ersten n ungeraden Zahlen ist n^2
-Ü Was ist die Summe der ersten 100 ungeraden Zahlen?
-            </pre>,
+    createNumberKeyboardExercise({
+        body: input => <>
+            <p>Die Summe der ersten {mathSpan("n")} ungeraden Zahlen ist {mathSpan("n^2")}.</p>
+            <p>Was ist die Summe der ersten 100 ungeraden Zahlen?</p>
+            <p>{mathDiv("#sum_{i=1}^{100}(2i+1) = #textcolor{blue}{" + (input || "?") + "}")}</p>
+        </>,
+        correct: 10000,
     }),
     createReadStep({
         content: <pre>
