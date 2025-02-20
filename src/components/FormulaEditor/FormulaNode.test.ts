4d804bd040ea0b7f1c51c4fca2b88170bb19e417
Martin Geisse <martingeisse@googlemail.com>
2025-02-20 13:23:45 +0100

    uni

--- src/components/FormulaEditor/FormulaNode.test.ts
+++ src/components/FormulaEditor/FormulaNode.test.ts
@@ -14,21 +14,36 @@ sure the value isn't null. We have to do it this way because expect() does not g
 type system.
  */
 
+function atom(content: string) {
+    return new Atom(content);
+}
+
+function seq(...nodes: FormulaNode[]) {
+    return new SequenceNode(nodes);
+}
+
+function sumOf(a: FormulaNode, b: FormulaNode, c: FormulaNode) {
+    return new SumNode([a, b, c]);
+}
+
+function sum() {
+    return sumOf(seq(), seq(), seq());
+}
+
+
 it("sequence with sum with empty content", () => {
-    const node = new SequenceNode([new SumNode([new SequenceNode(), new SequenceNode(), new SequenceNode()])]);
+    const node = seq(sum());
     checkForwardPositions(node, [[0], [0, 0, 0], [0, 1, 0], [0, 2, 0], [1]]);
     checkBackwardPositions(node, [[1], [0, 2, 0], [0, 1, 0], [0, 0, 0], [0]]);
     // checkInsert(node, [0], new SequenceNode([new Atom("a"), new SumNode([new SequenceNode(), new SequenceNode(), new SequenceNode()])]));
 });
 
 it("sequence with sum with non-empty content", () => {
-    const node = new SequenceNode([
-        new SumNode([
-            new SequenceNode([new Atom("a"), new Atom("b")]),
-            new SequenceNode([new Atom("c"), new Atom("d")]),
-            new SequenceNode([new Atom("e"), new Atom("f")]),
-        ]),
-    ]);
+    const node = seq(sumOf(
+        seq(atom("a"), atom("b")),
+        seq(atom("c"), atom("d")),
+        seq(atom("e"), atom("f")),
+    ));
     checkForwardPositions(node, [[0], [0, 0, 0], [0, 0, 1], [0, 0, 2], [0, 1, 0], [0, 1, 1], [0, 1, 2],
         [0, 2, 0], [0, 2, 1], [0, 2, 2], [1]]);
     checkBackwardPositions(node, [[1], [0, 2, 2], [0, 2, 1], [0, 2, 0], [0, 1, 2], [0, 1, 1], [0, 1, 0],
@@ -36,17 +51,17 @@ it("sequence with sum with non-empty content", () => {
 });
 
 it("single toplevel sum formula with empty content", () => {
-    const node = new SumNode([new SequenceNode(), new SequenceNode(), new SequenceNode()]);
+    const node = sum();
     checkForwardPositions(node, [[0, 0], [1, 0], [2, 0]]);
     checkBackwardPositions(node, [[2, 0], [1, 0], [0, 0]]);
 });
 
 it("single toplevel sum formula with non-empty content", () => {
-    const node = new SumNode([
-        new SequenceNode([new Atom("a"), new Atom("b")]),
-        new SequenceNode([new Atom("c"), new Atom("d")]),
-        new SequenceNode([new Atom("e"), new Atom("f")]),
-    ]);
+    const node = sumOf(
+        seq(atom("a"), atom("b")),
+        seq(atom("c"), atom("d")),
+        seq(atom("e"), atom("f")),
+    );
     checkForwardPositions(node, [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]]);
     checkBackwardPositions(node, [[2, 2], [2, 1], [2, 0], [1, 2], [1, 1], [1, 0], [0, 2], [0, 1], [0, 0]]);
 });
