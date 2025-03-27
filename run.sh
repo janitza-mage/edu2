91760f665595ddfb8e8a1c4e9731e7c7f7d2473a
Martin Geisse <martin.geisse@janitza.de>
2025-03-27 10:30:43 +0100

    ...

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-npm run dev
+gnome-terminal --tab --title "3001" -- bash -c 'cd ; . .bashrc ; . .nvm/nvm.sh ; cd ~/git-repos/spot-problems ; PORT=3001 npm run dev'
+gnome-terminal --tab --title "3002" -- bash -c 'cd ; . .bashrc ; . .nvm/nvm.sh ; cd ~/git-repos/spot-problems ; PORT=3002 npm run dev'
--- src/problems/createProblem.tsx
+++ src/problems/createProblem.tsx
@@ -1,34 +1,13 @@
 import {Problem} from "./Problem";
-import {randomInt} from "../util/random/randomInt";
-import {createNumberKeyboardProblem} from "../components/NumberKeyboard/createNumberKeyboardProblem";
-import {mathDiv} from "../components/Math/Math";
-import {TextSize} from "../components/layout/TextSize";
-import {createImmediateFeedbackChoiceProblemShuffled} from "../components/Choice/createImmediateFeedbackChoiceProblem";
+import {createProblem3001} from "./createProblem3001";
+import {createProblem3002} from "./createProblem3002";
 
 export function createProblem(): Problem {
-    /*
-    const x = 2 + randomInt(8);
-    const y = 2 + randomInt(8);
-    return createNumberKeyboardProblem({
-        body: input => <TextSize size={4}>{mathDiv(x + " #cdot " + y + " = " + (input || "?"))}</TextSize>,
-        validator: x * y,
-    });
-     */
-
-    
-    const x = 2 + randomInt(8);
-    const y = 2 + randomInt(8);
-    return createImmediateFeedbackChoiceProblemShuffled(
-        <TextSize size={4}>{mathDiv(x + " #cdot " + y + " = ")}</TextSize>,
-        x * y,
-        [
-            x * y + 5,
-            x * y - 5,
-            x * y + 7,
-            x * y - 7,
-        ],
-        {
-            variant: "inline",
-        },
-    );
+    if (window.location.href.includes(":3001/")) {
+        return createProblem3001();
+    }
+    if (window.location.href.includes(":3002/")) {
+        return createProblem3002();
+    }
+    return _props => <div>incorrect port</div>;
 }
