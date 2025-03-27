c1b5e186820fee88b094ed8a0ffd399917622b90
Martin Geisse <martin.geisse@janitza.de>
2025-03-27 09:59:35 +0100

    ...

--- src/problems/createProblem.tsx
+++ src/problems/createProblem.tsx
@@ -2,12 +2,13 @@ import {Problem} from "./Problem";
 import {randomInt} from "../util/random/randomInt";
 import {createNumberKeyboardProblem} from "../components/NumberKeyboard/createNumberKeyboardProblem";
 import {mathDiv} from "../components/Math/Math";
+import {TextSize} from "../components/layout/TextSize";
 
 export function createProblem(): Problem {
     const x = 2 + randomInt(8);
     const y = 2 + randomInt(8);
     return createNumberKeyboardProblem({
-        body: input => mathDiv(x + " #cdot " + y + " = " + (input || "?")),
+        body: input => <TextSize size={4}>{mathDiv(x + " #cdot " + y + " = " + (input || "?"))}</TextSize>,
         validator: x * y,
     });
 }
